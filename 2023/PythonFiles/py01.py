1f9963b0b9ae6ed98e714983593ce49de00856b9
unknown <769540542@qq.com>
2024-02-19 13:50:16 +0800

    gg

---
+++ 2023/PythonFiles/py01.py
@@ -0,0 +1,27 @@
+print("执行第1行啦")
+print(2 / 1)
+
+print("执行第3行啦")
+
+a = 123
+
+print(a)
+
+
+def add(left, right):
+    print("执行第9行啦")
+    print("执行第10行啦", left + right)
+
+
+print("执行第13行啦")
+
+add(3, 4)
+
+a = 124
+
+print(a)
+
+print("执行第17行啦")
+
+
+print("Hello wold")
